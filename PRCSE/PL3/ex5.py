d9360ba626178bd0167e5e8fa45996e9c1b7eae1
Marcelo Carvalho <mca@contentguru.com>
2023-11-22 15:15:55 +0000

    add ex5

--- PRCSE/PL3/ex5.py
+++ PRCSE/PL3/ex5.py
@@ -1,1 +1,14 @@
-
+'''
+Read a number from console. The number will be the length of the Fibonacci sequence to
+generate. Fibonacci is a sequence of numbers where the next number in the sequence is the sum
+of the previous two numbers in the sequence
+'''
+length = int(input('Fib size\n'))
+t1 = 0
+t2 = 1
+next_term = t1 + t2
+for i in range(length):
+    print(next_term)
+    t1 = t2
+    t2 = next_term
+    next_term = t1 + t2
