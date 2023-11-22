1b9d1f5d73b9d8d789d767409dfc29f7a85f800b
Marcelo Carvalho <mca@contentguru.com>
2023-11-22 09:38:49 +0000

    add ex1

---
+++ PRCSE/PL3/ex1.py
@@ -0,0 +1,14 @@
+'''
+ Read a number from console and determine if it is a perfect square number. A perfect square is
+a number that can be expressed as the product of an integer by itself or as the second exponent
+of an integer.
+    a. Hint: input() – built-in python function.
+'''
+import math
+number = int(input('Insert a number\n'))
+
+sqrt_number = math.sqrt(number)
+if sqrt_number.is_integer():
+    print("The number is a perfect square")
+else:
+    print("The number is not a perfect square")
