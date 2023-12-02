930eefb59b54d53f6bed962eebfbd16371fab508
Marcelo Carvalho <mca@contentguru.com>
2023-12-02 16:23:36 +0000

    ex11 done

--- PRCSE/PL3/ex11ToDo.py
+++ PRCSE/PL3/ex11ToDo.py
@@ -21,42 +21,41 @@ import json
 def fetch_weather_data(location_url):
     response = requests.get(location_url)
     soup = BeautifulSoup(response.content, 'html.parser')
-
     # Extract information under the "Detailed Forecast" section
     detailed_forecast = soup.find(id='detailed-forecast')
+    #Get times list
+    forecast_row = detailed_forecast.find_all("div", {"class": "row row-odd row-forecast"})
+    forecast_dictionary = {}
+    for row in forecast_row: 
+        forecast_label = row.find("div", {"class": "col-sm-2 forecast-label"})
+        forecast_text = row.find("div", {"class": "col-sm-10 forecast-text"})
+        forecast_dictionary[forecast_label.get_text(strip=True)] = forecast_text.get_text(strip=True)
+    print('Dic: ', forecast_dictionary)
+    # Get whether for the times
 
-    # Extract the text content of the "Detailed Forecast" section
-    detailed_forecast_text = detailed_forecast.get_text(strip=True)
-    print(f'det: {detailed_forecast_text}')
-    # Split the detailed forecast into sentences
-    forecast_sentences = [sentence.strip() for sentence in detailed_forecast_text.split('.') if sentence]
-    #print(f'Content: {forecast_sentences}')
-    # Extract relevant information for Today and Tonight
-    today_forecast = forecast_sentences[0]
-    tonight_forecast = forecast_sentences[1]
 
-    # Create the desired JSON structure
-    extracted_data = {
-        'Today': today_forecast,
-        'Tonight': tonight_forecast
-    }
-
-    return extracted_data
+    return forecast_dictionary
 
 def save_to_json(data, output_file):
     with open(output_file, 'w') as json_file:
         json.dump(data, json_file, indent=2)
 
-if __name__ == "__main__":
-    location_url = 'https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168'
-    output_file = 'weather_data.json'
+def get_value_from_json(json_file_path, key):
+    try:
+        # Read the JSON file
+        with open(json_file_path, 'r') as json_file:
+            data = json.load(json_file)
 
-    weather_data = fetch_weather_data(location_url)
-    save_to_json(weather_data, output_file)
+        # Check if the key exists in the dictionary
+        if key in data:
+            return data[key]
+        else:
+            print(f"The key '{key}' does not exist in the JSON file.")
+            return None
 
-    print(f'The weather data has been successfully fetched and saved to {output_file}.')
-
-import json
+    except FileNotFoundError:
+        print(f"File not found: {json_file_path}")
+        return None
 
 def get_weather_forecast(json_file, day):
     with open(json_file, 'r') as json_file:
@@ -70,9 +69,15 @@ def get_weather_forecast(json_file, day):
     return weather_forecast
 
 if __name__ == "__main__":
+    location_url = 'https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168'
+    output_file = 'weather_data.json'
+
+    weather_data = fetch_weather_data(location_url)
+    save_to_json(weather_data, output_file)
+
     json_file = 'weather_data.json'
     user_input_day = input('Enter the day for the weather forecast (e.g., "Today", "Tonight", "Wednesday"): ')
 
-    weather_forecast = get_weather_forecast(json_file, user_input_day)
+    weather_forecast = get_value_from_json(json_file, user_input_day)
     print(f'Weather Forecast for {user_input_day}: {weather_forecast}')
 
