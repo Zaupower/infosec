e5d69d3ea199c34a07f58f0e4718776941fa29df
Marcelo Carvalho <mca@contentguru.com>
2023-12-05 21:37:41 +0000

    ad t2

---
+++ T2/ex3.py
@@ -0,0 +1,15 @@
+
+from stegano import lsb
+from PIL import Image
+
+
+def decode_lsb(image_path):
+    clear_message = lsb.reveal(image_path)
+    return clear_message
+
+# Example usage
+output_image_path = "path/to/your/output/ips_encoded.png"
+
+# Decode message from encoded image
+decoded_message = decode_lsb(output_image_path)
+print("Decoded Message:", decoded_message)
