984de67522612877905d633f9414915f5d085084
Marcelo <marcelomascfb22@gmail.com>
2023-12-07 22:30:45 +0000

    add t3

--- T2/ex3.py
+++ T2/ex3.py
@@ -1,4 +1,4 @@
-
+import sys
 from stegano import lsb
 from PIL import Image
 
@@ -12,4 +12,32 @@ output_image_path = "PRCSE-C2.png"
 
 # Decode message from encoded image
 decoded_message = decode_lsb(output_image_path)
-print("Decoded Message:", decoded_message)
+
+
+def caesar_cipher(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            ascii_offset = ord('A') if char.isupper() else ord('a')
+            result += chr((ord(char) - ascii_offset + shift) % 26 + ascii_offset)
+        else:
+            result += char
+    return result
+
+def bruteforce_ceaser(text, number_of_tries):
+    for i in range(number_of_tries):
+        print(str(i))
+        print( caesar_cipher(text, i))
+
+# Decode message from encoded image
+decoded_message = decode_lsb(output_image_path)
+if decoded_message:
+    print("Message found: " + decoded_message)
+    tries = int(input("Please input the number of tries to bruteforce with ceaser cypher\n"))
+    bruteforce_ceaser(decoded_message, tries)
+else:
+    print("No message found in image")
+
+
+
+
