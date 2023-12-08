3d7a17acec49843269914f31330caf030c62f682
Marcelo <marcelomascfb22@gmail.com>
2023-12-08 21:45:16 +0000

    add pk

--- ex2/decryptor.py
+++ ex2/decryptor.py
@@ -17,20 +17,15 @@ def scanRecurse(baseDir):
             yield entry
         else:
             yield from scanRecurse(entry.path)
-
-
 def decrypt(dataFile, privateKeyFile, extension):
-    '''
-    use EAX mode to allow detection of unauthorized modifications
-    '''
-
+    #use EAX mode to allow detection of unauthorized modifications
     # read private key from file
     extension = dataFile.suffix.lower()
     with open(privateKeyFile, 'rb') as f:
         privateKey = f.read()
         # create private key object
         key = RSA.import_key(privateKey)
-
+        
     # read data from file
     with open(dataFile, 'rb') as f:
         # Read the session key
@@ -51,29 +46,17 @@ def decrypt(dataFile, privateKeyFile, extension):
     # save the decrypted data to file
     dataFile = str(dataFile)
     fileName= dataFile.split(extension)[0]
-    fileExtension = '.decrypted' # mark the file was decrypted
-    #decryptedFile = fileName + fileExtension
     decryptedFile = fileName + "-DECRIPTED-" + extension
     with open(decryptedFile, 'wb') as f:
         f.write(data)
-
     print('Decrypted file saved to ' + decryptedFile)
 
-directory = '/home/marcelo/Documents/infosec/ex2/TestRamsware' # CHANGE THIS
+directory = '/home/marcelo/Documents/infosec/ex2/TestRamsware' 
 
-# BONUS for you
 dir = input('put your directory (default is "TestRamsware" ):')
 if dir:
   directory = dir
-
-
-# because we need to decrypt file focus on .L0v3sh3 extension here is the code
-includeExtension = ['.l0v3sh3'] # CHANGE THIS make sure all is lower case
-
 for item in scanRecurse(directory): 
     filePath = Path(item)
     fileType = filePath.suffix.lower()
-    # run the decryptor just if the extension is .l0v3sh3
-    #if fileType in includeExtension:
-      #print(Path(filePath)) # testing the scanning file
     decrypt(filePath, privateKeyFile, fileType)
